787acc5e6728234e04e41833ffae9437eefc3be8
Kataloq <mrzema9922@gmail.com>
2024-12-23 22:47:24 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
+// Şəkillər üçün "overlay" funksiyası
 document.querySelectorAll('.product img').forEach(img => {
     img.addEventListener('click', () => {
         const overlay = document.createElement('div');
         overlay.className = 'overlay';
         overlay.innerHTML = `<img src="${img.src}" alt="${img.alt}">`;
         document.body.appendChild(overlay);
+
+        // Böyük şəkil modundan çıxmaq üçün klik
         overlay.addEventListener('click', () => overlay.remove());
     });
 });
